5d416924b83c548acef5fa10be644efba942c097
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-07 23:05:37 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let starti = 0; starti < word.length / 2; starti++) {
+    const endi= word.length - 1 - starti;
+    if (word[starti] !== word[endi]) {
+      
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  comparing the letters form start and from end till reach the middle one
 */
 
 /*
   Add written explanation of your solution here
+  
 */
 
 // You can run `node index.js` to view these console logs
